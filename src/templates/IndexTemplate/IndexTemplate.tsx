48a70668e437d7ad8f5bcd7540186afa3d5d4c7a
Charles Hood <charles.hood@gmail.com>
2022-11-17 14:32:15 -0500

    Update IndexTemplate.tsx
    hack to display static content instead of posts

--- src/templates/IndexTemplate/IndexTemplate.tsx
+++ src/templates/IndexTemplate/IndexTemplate.tsx
@@ -2,79 +2,71 @@ import React from "react";
 
 import { graphql } from "gatsby";
 
-import { Feed } from "@/components/Feed";
 import { Layout } from "@/components/Layout";
 import { Meta } from "@/components/Meta";
 import { Page } from "@/components/Page";
-import { Pagination } from "@/components/Pagination";
 import { Sidebar } from "@/components/Sidebar";
 import { useSiteMetadata } from "@/hooks";
-import { AllMarkdownRemark, PageContext } from "@/types";
+import { Node } from "@/types";
 
 interface Props {
   data: {
-    allMarkdownRemark: AllMarkdownRemark;
+    markdownRemark: Node;
   };
-  pageContext: PageContext;
 }
 
-const IndexTemplate: React.FC<Props> = ({ data, pageContext }: Props) => {
-  const { pagination } = pageContext;
-  const { hasNextPage, hasPrevPage, prevPagePath, nextPagePath } = pagination;
-
-  const { edges } = data.allMarkdownRemark;
+const PageTemplate: React.FC<Props> = ({ data }: Props) => {
+  const { html: body } = data.markdownRemark;
+  const { frontmatter } = data.markdownRemark;
+  const { title } = frontmatter;
 
   return (
     <Layout>
-      <Sidebar isIndex />
-      <Page>
-        <Feed edges={edges} />
-        <Pagination
-          prevPagePath={prevPagePath}
-          nextPagePath={nextPagePath}
-          hasPrevPage={hasPrevPage}
-          hasNextPage={hasNextPage}
-        />
+      <Sidebar />
+      <Page title={title}>
+        <div dangerouslySetInnerHTML={{ __html: body }} />
       </Page>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query IndexTemplate($limit: Int!, $offset: Int!) {
-    allMarkdownRemark(
-      limit: $limit
-      skip: $offset
-      sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
-    ) {
-      edges {
-        node {
-          fields {
-            categorySlug
-            slug
-          }
-          frontmatter {
-            description
-            category
-            title
-            date
-            slug
-          }
+  query PageTemplate {
+    markdownRemark(frontmatter: { template: { eq: "index" } }) {
+      id
+      html
+      frontmatter {
+        title
+        date
+        description
+        socialImage {
+          publicURL
         }
       }
     }
   }
 `;
 
-export const Head: React.FC<Props> = ({ pageContext }) => {
-  const { title, subtitle } = useSiteMetadata();
-  const {
-    pagination: { currentPage: page },
-  } = pageContext;
-  const pageTitle = page > 0 ? `Posts - Page ${page} - ${title}` : title;
+export const Head: React.FC<Props> = ({ data }) => {
+  const { title, subtitle, url } = useSiteMetadata();
 
-  return <Meta title={pageTitle} description={subtitle} />;
+  const {
+    frontmatter: {
+      title: pageTitle,
+      description: pageDescription = "",
+      socialImage,
+    },
+  } = data.markdownRemark;
+  const description = pageDescription || subtitle;
+  const image = socialImage?.publicURL && url.concat(socialImage?.publicURL);
+
+  return (
+    <Meta
+      title={`${pageTitle} - ${title}`}
+      description={description}
+      image={image}
+    />
+  );
 };
 
-export default IndexTemplate;
+export default PageTemplate;
